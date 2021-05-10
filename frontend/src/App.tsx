022bdb88ef98bbbcef6bb9742bfb3713b4a665ec
Cesar Eduardo Sturmer <cesar_dudutm@hotmail.com>
2021-05-10 13:47:50 -0300

    end of the first class

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import DataTable from './components/DataTable';
+import Barchart from './components/BarChart';
+import DonutChart from './components/DonutChart';
 
 
 function App() {
+
   return (
-    
-    <h2 className="text-primary">Cesar</h2>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">DashBoard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+            <Barchart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+
+        <DataTable />
+      
+      
+      </div>
+      <Footer />
+    </>
 
 
   );
