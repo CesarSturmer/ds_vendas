41fadbe633bfa20b2fb942428ac1c2ba31726a3e
Cesar Eduardo Sturmer <cesar_dudutm@hotmail.com>
2021-05-13 22:32:28 -0300

    Env var

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,2 +1,4 @@
-export const BASE_URL = 'http://localhost:8080';
+//vai verificar se existe a variável de ambiente caso n tiver ?? roda no local. No provedor ele roda com variavel definida com endereço do heroku.
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
 
